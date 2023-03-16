1d92c35873ce1fc25d014707916f346a7bfc23da
André Cruz <acruz@cloudflare.com>
2023-03-16 17:42:22 +0000

    working version

---
+++ README.md
@@ -0,0 +1,8 @@
+# email_sender_worker
+
+An example worker that sends emails.
+
+## Wrangler
+
+You need a pre-release version of Wrangler to build this worker. The send email bindings are not yet released as well, so
+an unsafe bindings block needs to be used.
--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { EmailMessage } from "cloudflare:email";
+import { createMimeMessage } from "mimetext";
+
+function toReadableStream(value) {
+  return new ReadableStream({
+    start(controller) {
+      var enc = new TextEncoder();
+      controller.enqueue(enc.encode(value));
+      controller.close();
+    },
+  });
+}
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
-	},
+  async fetch(request, env) {
+    const msg = createMimeMessage();
+    msg.setSender({ name: "GPT-4", addr: "something@email.speedcf.com" });
+    msg.setRecipient("teste@cabine.org");
+    msg.setSubject("An email generated in a worker");
+    msg.setMessage(
+      "text/plain",
+      `Congratulations, you just sent an email from a worker.
+
+good bye.`
+    );
+
+    var message = new EmailMessage(
+      "something@email.speedcf.com",
+      "teste@cabine.org",
+      toReadableStream(msg.asRaw())
+    );
+    try {
+      await env.SEB.send(message);
+    } catch (e) {
+      return new Response(e.message);
+    }
+
+    return new Response("Hello Send Email World!");
+  },
 };
